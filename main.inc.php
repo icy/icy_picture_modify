d80297ddbb051a853af41ce1fcbdb4628588a4a0
icy <kyanh@viettug.org>
2011-07-04 06:17:32 +0700

    Fix the version number in the code

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Icy Modify Picture
-Version: 1.0.0
+Version: 1.0.1
 Description: Allow users to modify users they uploaded
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=563
 Author: icy
