0516b4eda2f50cee70c5ce78e8f79005340f46e5
Ky-Anh Huynh <kyanh@theslinux.org>
2013-08-14 23:26:15 +0700

    Bump new version 2.4.1

--- main.inc.php
+++ main.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Icy Modify Picture
-Version: 2.4.0
+Version: 2.4.1
 Description: Allow normal users to upload / modify pictures
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=563
 Author: icy
